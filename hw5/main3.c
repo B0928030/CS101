656d5a0336129b643371037f852761c875a61e78
B0928030 <winnie200109271@gmail.com>
2024-12-05 14:38:40 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    for(int i = 11; i < 100; i++){
+        if(i%10 == 0){
+            printf("\n");
+        }
+        else{
+            printf("%d*%d=%d\t",i/10,i%10,(i/10)*(i%10));    
+        }
+        
+    }
+
+    return 0;
+}
