36c678e8ac49ea267d239f6aecf4842ae9a6452e
B0928030 <winnie200109271@gmail.com>
2024-12-05 14:19:05 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 7;
+    for(int j = i; j > 0; j--){
+        for(int k = 1; k <= 2*i - j ; k++){
+            if(k >= j){
+                if((k-j)%2 == 0){
+                    printf("%d",(i-j+1));
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            else
+                printf(" ");
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
